2a8012fd912fa4f5cdd59337840c58720e60e2f7
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2016-01-04 21:14:21 +0900

    v0.0.10

--- tekyome.php
+++ tekyome.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <?php
-// 09 フォームに合わせてposition修正、フォームに指定したURLの画像を利用可能に、顔ラボに渡す時にURLエンコード
+// 10 注意のフォントを小さい灰色に、デフォルトで画像を表示しない、スポンサーのサイズ変更追加
 class Face {
 	public $rx;
 	public $ry;
@@ -13,19 +13,19 @@ class Face {
 
 if(isset($_GET['url']) && $_GET['url']!="") {
 	$imageUrl = $_GET['url'	];
+	$xml = simplexml_load_file('https://kaolabo.com/api/detect?apikey=15ff38a942356b754466cdf3735862a7&url='.urlencode($imageUrl)) or die("XMLパースエラー");
+	for ($iCounter = 0; $iCounter< count($xml->faces->face); $iCounter++){
+		$face[$iCounter] = new Face();
+		$face[$iCounter]->rx = $xml->faces->face[$iCounter]->{'right-eye'}['x'];
+		$face[$iCounter]->ry = $xml->faces->face[$iCounter]->{'right-eye'}['y'];
+		$face[$iCounter]->lx = $xml->faces->face[$iCounter]->{'left-eye'}['x'];
+		$face[$iCounter]->ly = $xml->faces->face[$iCounter]->{'left-eye'}['y'];
+		$face[$iCounter]->interval = sqrt(pow($face[$iCounter]->lx-$face[$iCounter]->rx,2)+pow($face[$iCounter]->ly-$face[$iCounter]->ry,2));
+		$face[$iCounter]->size = $face[$iCounter]->interval/2;
+		$face[$iCounter]->rotation = atan2($face[$iCounter]->ly-$face[$iCounter]->ry,$face[$iCounter]->lx-$face[$iCounter]->rx);
+	}
 } else {
-	$imageUrl = 'http://ec2.images-amazon.com/images/I/51-z7BZTYrL.jpg';
-}
-$xml = simplexml_load_file('https://kaolabo.com/api/detect?apikey=15ff38a942356b754466cdf3735862a7&url='.urlencode($imageUrl)) or die("XMLパースエラー");
-for ($iCounter = 0; $iCounter< count($xml->faces->face); $iCounter++){
-	$face[$iCounter] = new Face();
-	$face[$iCounter]->rx = $xml->faces->face[$iCounter]->{'right-eye'}['x'];
-	$face[$iCounter]->ry = $xml->faces->face[$iCounter]->{'right-eye'}['y'];
-	$face[$iCounter]->lx = $xml->faces->face[$iCounter]->{'left-eye'}['x'];
-	$face[$iCounter]->ly = $xml->faces->face[$iCounter]->{'left-eye'}['y'];
-	$face[$iCounter]->interval = sqrt(pow($face[$iCounter]->lx-$face[$iCounter]->rx,2)+pow($face[$iCounter]->ly-$face[$iCounter]->ry,2));
-	$face[$iCounter]->size = $face[$iCounter]->interval/2;
-	$face[$iCounter]->rotation = atan2($face[$iCounter]->ly-$face[$iCounter]->ry,$face[$iCounter]->lx-$face[$iCounter]->rx);
+	$imageUrl = '';
 }
 ?>
 <html>
@@ -45,9 +45,16 @@ h2 {color:#ffffff; background:#336699; padding:5px}
 	left:0px;
 	text-shadow:0px 0px 3px #000000;
 }
+#result {
+	color:#F00;
+}
+.attention {
+	font-size:small;
+	color:#999;
+}
 <?php
 for ($iCounter = 0; $iCounter<count($face); $iCounter++){
-print('#tei'.$iCounter.' {
+	print('#tei'.$iCounter.' {
 	position: absolute;
 	left:'.($face[$iCounter]->rx-$face[$iCounter]->size/2).'px;
 	top:'.($face[$iCounter]->ry-$face[$iCounter]->size/2).'px;
@@ -56,7 +63,7 @@ print('#tei'.$iCounter.' {
 	-webkit-transform: rotate('.($face[$iCounter]->rotation).'rad);
 	-o-transform: rotate('.($face[$iCounter]->rotation).'rad);
 	-ms-transform: rotate('.($face[$iCounter]->rotation).'rad);
-}
+	}
 #kyo'.$iCounter.' {
 	position: absolute;
 	left:'.($face[$iCounter]->lx-$face[$iCounter]->size/2).'px;
@@ -66,7 +73,10 @@ print('#tei'.$iCounter.' {
 	-webkit-transform: rotate('.($face[$iCounter]->rotation).'rad);
 	-o-transform: rotate('.($face[$iCounter]->rotation).'rad);
 	-ms-transform: rotate('.($face[$iCounter]->rotation).'rad);
-}
+	}
+#sponsor'.$iCounter.' {
+	font-size:'.($face[$iCounter]->size).'px;
+	}
 ');
 }
 ?>
@@ -77,6 +87,7 @@ $(function(){
 for ($iCounter = 0; $iCounter<count($face); $iCounter++){
 	print('	$("#sponsor'.$iCounter.'").css({
 		"position": "absolute",
+		"font-size":"'.($face[$iCounter]->size).'px",
 		"left": ('.(-sin($face[$iCounter]->rotation)*$face[$iCounter]->interval*1+($face[$iCounter]->rx+$face[$iCounter]->lx)/2).'-$(\'#sponsor'.$iCounter.'\').width()/2)+"px",
 		"top": ('.(cos($face[$iCounter]->rotation)*$face[$iCounter]->interval*1+($face[$iCounter]->ry+$face[$iCounter]->ly)/2).'-$(\'#sponsor'.$iCounter.'\').height()/2)+"px",
 		"-moz-transform": "rotate('.($face[$iCounter]->rotation).'rad)",
@@ -84,6 +95,9 @@ for ($iCounter = 0; $iCounter<count($face); $iCounter++){
 		"-o-transform": "rotate('.($face[$iCounter]->rotation).'rad)",
 		"-ms-transform": "rotate('.($face[$iCounter]->rotation).'rad)"
 	})'."\n");
+}
+if(count($face)==0 && $imageUrl!="") {
+	print('$("#result").text("顔が検出されませんでした。")');
 }
 ?>
 });
@@ -93,9 +107,10 @@ for ($iCounter = 0; $iCounter<count($face); $iCounter++){
 <body>
 <h1>tekyome!</h1>
 <form name="form1" method="get" action="<?php echo $_SERVER['SCRIPT_NAME'] ?>">
-	<input id="urlText" type="url" name="url" size="70" value="<?php print($imageUrl) ?>" />
-    <input type="submit" value="生成！" />
+	<input id="urlText" type="url" name="url" size="70" value="<?php print(($imageUrl!="")? $imageUrl : 'http://ec2.images-amazon.com/images/I/51-z7BZTYrL.jpg') ?>" />
+	<input type="submit" value="生成！" />
 </form>
+<div id="result"></div>
 <div class="stage">
 	<img class="picture" src="<?php echo($imageUrl) ?>" />
 <?php
@@ -109,7 +124,7 @@ for ($iCounter = 0; $iCounter< count($xml->faces->face); $iCounter++){
 }
 ?>
 </div>
-<div>※注意 画像を保存しても提供は付きません。</div>
+<div class="attention">※注意 画像を保存しても提供は付きません。</div>
 <div><a href="http://butchi.jp">Home</a></div>
 </body>
 </html>
