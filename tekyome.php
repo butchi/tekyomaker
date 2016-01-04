e77fc7db0414c3d5783b16a978b22c6137fff542
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2016-01-04 21:13:07 +0900

    v0.0.6

--- tekyome.php
+++ tekyome.php
@@ -1,14 +1,27 @@
+<!DOCTYPE html>
 <?php
-// 2012年5月3日 13:30-15:00
+class Face {
+	public $rx;
+	public $ry;
+	public $lx;
+	public $ly;
+	public $interval;
+	public $size;
+	public $rotation;
+}
+
 $imageUrl = 'http://ec2.images-amazon.com/images/I/51-z7BZTYrL._SS500_.jpg';
 $xml = simplexml_load_file('https://kaolabo.com/api/detect?apikey=15ff38a942356b754466cdf3735862a7&url='.$imageUrl) or die("XMLパースエラー");
-$rx = $xml->faces->face[0]->{'right-eye'}['x'];
-$ry = $xml->faces->face[0]->{'right-eye'}['y'];
-$lx = $xml->faces->face[0]->{'left-eye'}['x'];
-$ly = $xml->faces->face[0]->{'left-eye'}['y'];
-$interval = sqrt(pow($lx-$rx,2)+pow($ly-$ry,2));
-$size = $interval/2;
-$rotation = atan2($ly-$ry,$lx-$rx);
+for ($iCounter = 0; $iCounter< count($xml->faces->face); $iCounter++){
+	$face[$iCounter] = new Face();
+	$face[$iCounter]->rx = $xml->faces->face[$iCounter]->{'right-eye'}['x'];
+	$face[$iCounter]->ry = $xml->faces->face[$iCounter]->{'right-eye'}['y'];
+	$face[$iCounter]->lx = $xml->faces->face[$iCounter]->{'left-eye'}['x'];
+	$face[$iCounter]->ly = $xml->faces->face[$iCounter]->{'left-eye'}['y'];
+	$face[$iCounter]->interval = sqrt(pow($face[$iCounter]->lx-$face[$iCounter]->rx,2)+pow($face[$iCounter]->ly-$face[$iCounter]->ry,2));
+	$face[$iCounter]->size = $face[$iCounter]->interval/2;
+	$face[$iCounter]->rotation = atan2($face[$iCounter]->ly-$face[$iCounter]->ry,$face[$iCounter]->lx-$face[$iCounter]->rx);
+}
 ?>
 <html>
 <meta charset="utf-8">
@@ -16,25 +29,63 @@ $rotation = atan2($ly-$ry,$lx-$rx);
 <script src="jquery-1.7.2.min.js"></script>
 <style>
 .picture { position: absolute; left: 0px; top: 0px; }
-.teikyo { position: absolute; color: white; text-shadow: 0px 0px 3px #000000; font-size: <?php echo($size) ?>px;
- -moz-transform: rotate(<?php echo($rotation) ?>rad);
- -webkit-transform: rotate(<?php echo($rotation) ?>rad);
- -o-transform: rotate(<?php echo($rotation) ?>rad);
- -ms-transform: rotate(<?php echo($rotation) ?>rad); }
-.sponsor { left: <?php echo(($rx+$lx)/2-50) ?>px; top: <?php echo(($ry+$ly)/2+$interval) ?>px;}
+.teikyo {
+	position:absolute;
+	color:white;
+	top:0px;
+	left:0px;
+	text-shadow:0px 0px 3px #000000;
+}
+.sponsor {  }
+<?php
+for ($iCounter = 0; $iCounter<count($face); $iCounter++){
+print('#tei'.$iCounter.' {
+	position: absolute;
+	left:'.($face[$iCounter]->rx-$face[$iCounter]->size/2).'px;
+	top:'.($face[$iCounter]->ry-$face[$iCounter]->size/2).'px;
+	font-size:'.($face[$iCounter]->size).'px;
+	-moz-transform: rotate('.($face[$iCounter]->rotation).'rad);
+	-webkit-transform: rotate('.($face[$iCounter]->rotation).'rad);
+	-o-transform: rotate('.($face[$iCounter]->rotation).'rad);
+	-ms-transform: rotate('.($face[$iCounter]->rotation).'rad);
+}
+#kyo'.$iCounter.' {
+	position: absolute;
+	left:'.($face[$iCounter]->lx-$face[$iCounter]->size/2).'px;
+	top:'.($face[$iCounter]->ly-$face[$iCounter]->size/2).'px;
+	font-size:'.($face[$iCounter]->size).'px;
+	-moz-transform: rotate('.($face[$iCounter]->rotation).'rad);
+	-webkit-transform: rotate('.($face[$iCounter]->rotation).'rad);
+	-o-transform: rotate('.($face[$iCounter]->rotation).'rad);
+	-ms-transform: rotate('.($face[$iCounter]->rotation).'rad);
+}
+#sponsor'.$iCounter.' {
+	position: absolute;
+	left: '.($face[$iCounter]->rx+$face[$iCounter]->interval/2).'px;
+	top: '.(($face[$iCounter]->ry+$face[$iCounter]->ly)/2+$face[$iCounter]->interval).'px;
+	-moz-transform: rotate('.($face[$iCounter]->rotation).'rad);
+	-webkit-transform: rotate('.($face[$iCounter]->rotation).'rad);
+	-o-transform: rotate('.($face[$iCounter]->rotation).'rad);
+	-ms-transform: rotate('.($face[$iCounter]->rotation).'rad);
+}
+');
+}
+?>
 </style>
 </head>
 <body>
 <div class="stage">
-  <img class="picture" src="<?php echo($imageUrl) ?>" />
-  <div class="teikyo tei">提</div>
-  <div class="teikyo kyo">供</div>
-  <div class="teikyo sponsor">butchi.jp</div>
+	<img class="picture" src="<?php echo($imageUrl) ?>" />
+<?php
+for ($iCounter = 0; $iCounter< count($xml->faces->face); $iCounter++){
+	print('	<div class="teikyo" id="teikyo'.$iCounter.'">');
+	print('<div class="tei" id="tei'.$iCounter.'">提</div>');
+	print('<div class="kyo" id="kyo'.$iCounter.'">供</div>');
+	print('<div class="sponsor" id="sponsor'.$iCounter.'" style="top:0px; left:0px">butchi.jp</div>');
+	print('</div>');
+	print("\n");
+}
+?>
 </div>
-<script>
-$(".tei").css({ left: "<?php echo($rx-$size/2) ?>px", top: "<?php echo($ry-$size/2) ?>px" });
-$(".kyo").css({ left: "<?php echo($lx-$size/2) ?>px", top: "<?php echo($ly-$size/2) ?>px" });
-$(".sponsor").css("left",<?php echo($rx+$interval/2) ?>-$(".sponsor").width()/2);
-</script>
 </body>
 </html>
