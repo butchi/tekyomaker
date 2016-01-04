93f2f0cb501ddd28d86605c926e18fd1c204f53e
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2016-01-04 21:10:42 +0900

    v0.0.4

--- tekyome.php
+++ tekyome.php
@@ -1,11 +1,11 @@
 <?php
-// 2012年4月29日 24:00-24:30
-$imageUrl = 'http://www.wallpaperlink.com/images/wallpaper/2007/0705/03414x.jpg';
+// 2012年5月3日 13:30-15:00
+$imageUrl = 'http://ec2.images-amazon.com/images/I/51-z7BZTYrL._SS500_.jpg';
 $xml = simplexml_load_file('https://kaolabo.com/api/detect?apikey=15ff38a942356b754466cdf3735862a7&url='.$imageUrl) or die("XMLパースエラー");
-$rx = $xml->faces[0]->face->{'right-eye'}['x'];
-$ry = $xml->faces[0]->face->{'right-eye'}['y'];
-$lx = $xml->faces[0]->face->{'left-eye'}['x'];
-$ly = $xml->faces[0]->face->{'left-eye'}['y'];
+$rx = $xml->faces->face[0]->{'right-eye'}['x'];
+$ry = $xml->faces->face[0]->{'right-eye'}['y'];
+$lx = $xml->faces->face[0]->{'left-eye'}['x'];
+$ly = $xml->faces->face[0]->{'left-eye'}['y'];
 $interval = sqrt(pow($lx-$rx,2)+pow($ly-$ry,2));
 $size = $interval/2;
 $rotation = atan2($ly-$ry,$lx-$rx);
@@ -21,18 +21,20 @@ $rotation = atan2($ly-$ry,$lx-$rx);
  -webkit-transform: rotate(<?php echo($rotation) ?>rad);
  -o-transform: rotate(<?php echo($rotation) ?>rad);
  -ms-transform: rotate(<?php echo($rotation) ?>rad); }
-.tei { left: <?php echo($rx-$size/2) ?>px; top: <?php echo($ry-$size/2) ?>px; }
-.kyo { left: <?php echo($lx-$size/2) ?>px; top: <?php echo($ly-$size/2) ?>px; }
 .sponsor { left: <?php echo(($rx+$lx)/2-50) ?>px; top: <?php echo(($ry+$ly)/2+$interval) ?>px;}
 </style>
 </head>
 <body>
-<img class="picture" src="<?php echo($imageUrl) ?>" />
-<div class="teikyo tei">提</div>
-<div class="teikyo kyo">供</div>
-<div id="sponsor1" class="teikyo sponsor">butchi.jp</div>
+<div class="stage">
+  <img class="picture" src="<?php echo($imageUrl) ?>" />
+  <div class="teikyo tei">提</div>
+  <div class="teikyo kyo">供</div>
+  <div class="teikyo sponsor">butchi.jp</div>
+</div>
 <script>
-$("#sponsor1").css("left",<?php echo($rx+$interval/2) ?>-$("#sponsor1").width()/2);
+$(".tei").css({ left: "<?php echo($rx-$size/2) ?>px", top: "<?php echo($ry-$size/2) ?>px" });
+$(".kyo").css({ left: "<?php echo($lx-$size/2) ?>px", top: "<?php echo($ly-$size/2) ?>px" });
+$(".sponsor").css("left",<?php echo($rx+$interval/2) ?>-$(".sponsor").width()/2);
 </script>
 </body>
 </html>
