5139f4358d21842f85a09baf0fcb4d818a6ea15e
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2016-01-04 21:18:50 +0900

    v0.0.19

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
 <script src="jquery-1.7.2.min.js"></script>
 <?php
-// 18 ソーシャルメディア対応、URLからindex.phpを除去（相対パス）
+// 19 ログファイル書き出し、アクセス解析追加
 class Face {
 	public $rx;
 	public $ry;
@@ -15,11 +15,22 @@ class Face {
 	public $rotation;
 }
 
+// ログファイル書き出し
+{
+	$fp = fopen('log.txt',"a");
+	flock($fp,LOCK_EX); // 排他ロック
+	
+	fwrite($fp, date("Y/m/d G:i:s")."\t".$_SERVER["QUERY_STRING"]."\n");
+	
+	flock($fp,LOCK_UN); // ロック開放
+	fclose($fp);
+}
+
 if(isset($_GET['imageUrl']) && $_GET['imageUrl']!='') {
 	$imageUrl = $_GET['imageUrl'];
 } else if(isset($_GET['twitterID']) && $_GET['twitterID']!='') {
-	$imageUrl = 'http://gadgtwit.appspot.com/twicon/'.urlencode($_GET['twitterID']).'/original';
-//	$imageUrl = 'http://api.osae.me/retwipi/'.urlencode($_GET['twitterID']).'/original';
+//	$imageUrl = 'http://gadgtwit.appspot.com/twicon/'.urlencode($_GET['twitterID']).'/original'; // エラー500 @2012/7/10
+	$imageUrl = 'http://api.osae.me/retwipi/'.urlencode($_GET['twitterID']).'/original';
 }
 
 if($imageUrl!='') {
@@ -142,6 +153,22 @@ if(count($face)==0 && $imageUrl!="") {
 });
 </script>
 <title>提供目ーカー</title>
+<!-- Google Analytics -->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-33131588-1']);
+  _gaq.push(['_setDomainName', 'onra.in']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<!-- /Google Analytics -->
 </head>
 <body>
 <!-- Facebook -->
