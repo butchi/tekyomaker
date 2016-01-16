991e6e7307a4cde7a63957a92ceaa8c11c17361a
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2016-01-16 10:59:54 +0900

    unite staircase

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,9 @@ window.licker = window.licker || {};
     var $listSponsor = $('.list-sponsor');
     var $sponsorTmpl = $listSponsor.find('.item-sponsor').eq(0).clone();
 
+    var $btnShooting = $('.btn-shooting button');
+    var $btnUpload = $('.btn-upload button');
+
     $('.btn-add-sponsor').on('click', function() {
       $listSponsor.append($sponsorTmpl.clone());
     });
@@ -16,128 +19,6 @@ window.licker = window.licker || {};
         $(this).closest('.item-sponsor').remove();
       }
     });
-
-    $('.form-main').on('submit', (evt) => {
-      evt.preventDefault();
-      submitHandler();
-    });
-
-    function submitHandler() {
-      var imageUrl = $('.intput-file').val();
-      console.log(imageUrl);
-
-      var client = new FCClientJS(ns.API_KEY, ns.API_SECRET);
-      var options = new Object();
-      // options.detect_all_feature_points = true;
-      client.facesDetect(imageUrl, null, options, callback);
-
-      function callback(data) {
-        console.log(data);
-
-        $(function(){
-          data.photos.forEach((photo) => {
-            var $stage = $('<div></div>').addClass('stage');
-            $stage.append(`<img class="picture" src="${imageUrl}">`);
-            $('.result').prepend($stage);
-
-            var width = photo.width;
-            var height = photo.height;
-
-            var faceArr = photo.tags;
-            faceArr.forEach((tag, idx) => {
-              var sponsorName = $listSponsor.find('.item-sponsor').eq(idx).find('.input-sponsor').val();
-
-              var $teikyo = $('<div></div>')
-              .addClass('teikyo');
-              var $tei = $('<span>提</span>')
-              .addClass('tei');
-              var $kyo = $('<span>供</span>')
-              .addClass('kyo');
-              var $sponsor = $('<div></div>')
-              .addClass('sponsor')
-              .text(sponsorName);
-              $teikyo.append($tei, $kyo, $sponsor);
-
-              var lx = tag.eye_left.x * width / 100;
-              var ly = tag.eye_left.y * height / 100;
-              var rx = tag.eye_right.x * width / 100;
-              var ry = tag.eye_right.y * height / 100;
-              var interval = Math.sqrt(Math.pow(lx - rx, 2)+Math.pow(ly - ry, 2));
-              var size = interval / 2;
-              var rotation = Math.atan2(ly - ry, lx - rx);
-
-              $tei.css({
-                position: 'absolute',
-                left: rx - size / 2,
-                top: ry - size / 2,
-                'font-size': size,
-                transform: `rotate(${rotation}rad)`,
-              });
-
-              $kyo.css({
-                position: 'absolute',
-                left: lx - size / 2,
-                top: ly - size / 2,
-                'font-size': size,
-                transform: `rotate(${rotation}rad)`,
-              });
-
-              $sponsor.css({
-                position: 'absolute',
-                width: '2000px',
-                left: (- Math.sin(rotation) * interval + (rx + lx) / 2) - 1000,
-                top:  (  Math.cos(rotation) * interval + (ry + ly) / 2) - 10,
-                'text-align': 'center',
-                'font-size': size + 'px',
-                transform: `rotate(${rotation}rad)`,
-              });
-
-              $stage.append($teikyo);
-
-              // if(count($face)==0 && $imageUrl!="") {
-              //   // 顔が検出されなかったとき
-              //   print(' <div class="teikyo" id="teikyo">
-              //   <div class="tei">提</div>
-              //   <div class="kyo">供</div>
-              //   <div class="sponsor">'.$_GET['sponsor'][0].'</div>
-              // </div>');
-              // }
-            });
-
-            // if(faceArr.length === 0) {
-            //   $("#result").text("顔が検出されませんでした。");
-            //   $(".picture").bind("load", function() {
-            //     var size = ($(".picture").width() < $(".picture").height())? $(".picture").width()*0.1 : $(".picture").width()*0.1;
-            //     $(".tei").css({
-            //       "position": "absolute",
-            //       "left": 0.4*$(this).width()-size/2+"px",
-            //       "top": 0.4*$(this).height()-size/2+"px",
-            //       "font-size": size+"px"
-            //     });
-            //     $(".kyo").css({
-            //       "position": "absolute",
-            //       "left": 0.6*$(this).width()-size/2+"px",
-            //       "top": 0.4*$(this).height()-size/2+"px",
-            //       "font-size": size+"px"
-            //     });
-            //     $(".sponsor").css({
-            //       "position": "absolute",
-            //       "width": $(".picture").width()+"px",
-            //       "top": 0.6*$(this).height()-size/2+"px",
-            //       "text-align": "center",
-            //       "font-size": size+"px"
-            //     });
-            //   });
-            // }
-          });
-        });
-      }
-    }
-  });
-
-  $(function media() {
-    var $btnShooting = $('.btn-shooting button');
-    var $btnUpload = $('.btn-upload button');
 
     var Events = Staircase.Events;
     Staircase.initialize();
@@ -165,6 +46,24 @@ window.licker = window.licker || {};
       $btnStartCamera.hide();
       $navigateCamera.hide();
     }
+
+    $('.btn-camera').on('click', () => {
+      $screenCamera.show();
+    });
+
+    $('.btn-upload').on('click', () => {
+      $screenUpload.show();
+    });
+
+    $('.btn-navigate-upload').on('click', () => {
+      $screenUpload.hide();
+      $screenCamera.show();
+    });
+
+    $('.btn-navigate-camera').on('click', () => {
+      $screenCamera.hide();
+      $screenUpload.show();
+    });
 
     // カメラ起動
     $btnStartCamera.on('click', (e) => {
@@ -216,18 +115,9 @@ window.licker = window.licker || {};
       camera.powerOn();
       $screenCamera.show();
     });
-  });
 
-  $(function test() {
-    // function blobToFile(theBlob, fileName){
-    //     //A Blob() is almost a File() - it's just missing the two properties below which we will add
-    //     theBlob.lastModifiedDate = new Date();
-    //     theBlob.name = fileName;
-    //     return theBlob;
-    // }
-
-    var $btn = $('.btn-draw');
-    $btn.on('click', () => {
+    $('.block-input .btn-generate').on('click', (evt) => {
+      console.info('generating');
       var canvas = ns.previewCanvas.getCanvas();
       var ctx = canvas.getContext('2d');
 
@@ -240,6 +130,7 @@ window.licker = window.licker || {};
       client.facesDetect(null, [blob], options, callback);
 
       function callback(data) {
+        console.log(data);
         JSON.parse(data).photos.forEach((photo) => {
           var width = photo.width;
           var height = photo.height;
