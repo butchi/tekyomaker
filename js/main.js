e5e0dd83983937387025838fcb263f02d3a05dea
IWABUCHI Yu(u)ki <butchiyu+com-github@gmail.com>
2023-01-29 02:09:21 +0900

    clmtrackr導入

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 import "https://code.jquery.com/jquery-3.6.3.min.js"
-import "https://cdn.jsdelivr.net/gh/cgarciagl/face-api.js@0.22.2/dist/face-api.min.js"
+import clm from "https://cdnjs.cloudflare.com/ajax/libs/clmtrackr/1.1.2/clmtrackr.module.min.js"
+
+const ctracker = new clm.tracker()
 
 const canvasElm = document.querySelector("canvas")
 const videoElm = document.querySelector("video")
 
 const initializeWithUserMedia = async _ => {
-  let landmarkArr = []
-
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       video: {
@@ -18,85 +18,59 @@ const initializeWithUserMedia = async _ => {
     videoElm.srcObject = stream
     videoElm.play()
 
+    ctracker.init();
+    ctracker.start(videoElm);
+
 
     const ctx = canvasElm.getContext("2d")
 
     const ticker = async _ => {
+      const posArr = ctracker.getCurrentPosition()
+      const eyeArr = [posArr[27], posArr[32]]
+
       ctx.drawImage(videoElm, 0, 0)
 
-      const personArr = landmarkArr
-        .map(l => [l.landmarks.getLeftEye(), l.landmarks.getRightEye()].map(eyeLmArr => eyeLmArr.reduce((p, c) => ({
-          x: p.x + c.x / eyeLmArr.length,
-          y: p.y + c.y / eyeLmArr.length,
-        }), { x: 0, y: 0 })))
 
       const ratio = 1
 
-      personArr.forEach(eyeArr => {
-        if (eyeArr == null) return
+      const [lx, ly] = eyeArr[0] || [null, null]
+      const [rx, ry] = eyeArr[1] || [null, null]
 
-        const lx = eyeArr[0].x
-        const ly = eyeArr[0].y
-        const rx = eyeArr[1].x
-        const ry = eyeArr[1].y
+      const interval = Math.sqrt(Math.pow(lx - rx, 2) + Math.pow(ly - ry, 2))
+      const size = interval / 2
+      const rotation = Math.atan2(ry - ly, rx - lx)
 
-        const interval = Math.sqrt(Math.pow(lx - rx, 2) + Math.pow(ly - ry, 2))
-        const size = interval / 2
-        const rotation = Math.atan2(ry - ly, rx - lx)
+      ctx.textAlign = "center"
+      ctx.textBaseline = "top"
 
-        ctx.textAlign = "center"
-        ctx.textBaseline = "top"
+      eyeArr.forEach((eye, idx) => {
+        const [x, y] = eye || [null, null]
 
-        eyeArr.forEach((eye, idx) => {
-          ctx.save()
+        ctx.save()
 
-          ctx.lineWidth = 6
-          ctx.lineJoin = "miter"
-          ctx.miterLimit = 5
-          ctx.strokeStyle = "black"
-          ctx.fillStyle = "white"
-          ctx.font = `bold ${size * ratio}px sans-serif`
+        ctx.lineWidth = 6
+        ctx.lineJoin = "miter"
+        ctx.miterLimit = 5
+        ctx.strokeStyle = "black"
+        ctx.fillStyle = "white"
+        ctx.font = `bold ${size * ratio}px sans-serif`
 
-          ctx.translate(eye.x, eye.y)
-          ctx.rotate(rotation)
+        ctx.translate(x, y)
+        ctx.rotate(rotation)
 
-          ctx.strokeText("提供"[idx], 0, - size * ratio / 2)
-          ctx.fillText("提供"[idx], 0, - size * ratio / 2)
+        ctx.strokeText("提供"[idx], 0, - size * ratio / 2)
+        ctx.fillText("提供"[idx], 0, - size * ratio / 2)
 
-          ctx.restore()
-        })
+        ctx.restore()
       })
 
       requestAnimationFrame(ticker)
     }
 
     ticker()
-
-    const detectTicker = async _ => {
-      const urlBase64 = canvasElm.toDataURL('image/png');
-
-      let img = await faceapi.fetchImage(urlBase64);
-      landmarkArr = await faceapi
-        .detectAllFaces(img)
-        .withFaceLandmarks()
-
-      detectTicker()
-    }
-
-    detectTicker()
-
   } catch (err) {
     console.log(err)
   }
 }
 
-$(globalThis).on("load", async _evt => {
-  await faceapi.loadFaceLandmarkModel("./js/lib/weights")
-  await faceapi.loadFaceRecognitionModel("./js/lib/weights")
-  await faceapi.nets.ssdMobilenetv1.loadFromUri("./js/lib/weights")
-
-  $(".loading").hide()
-
-  initializeWithUserMedia()
-})
-
+initializeWithUserMedia()
